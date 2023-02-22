37cb223ddbd94308536dcadc8346a4735900b42e
Fahmi Ramadhan <fahmi.ra@photon.in>
2023-02-22 10:48:03 +0700

    udpate

--- app/model/index.ts
+++
@@ -1,8 +0,0 @@
-const dbConfig = require("../config/database");
-const mongoose = require("mongoose");
-
-module.exports = {
-  mongoose,
-  url: dbConfig.urlMongoProd,
-  users: require("./users.model.ts")(mongoose),
-};
--- app/routes/users.routes.ts
+++
@@ -1,8 +0,0 @@
-module.exports = (app: any) => {
-  const users = require("../controller/users.controller");
-  const r = require("express").Router();
-
-  r.post("/", users.create);
-
-  app.use("/users", r);
-};
